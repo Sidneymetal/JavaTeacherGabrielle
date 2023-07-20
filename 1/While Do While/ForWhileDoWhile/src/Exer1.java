0b3266f85f6ea21d0ee3fed906de5ea1e02b83ad
Sidneymetal <sidneymetalop@gmail.com>
2023-07-20 12:18:10 -0300

    Iniciando repositório de exercícios Java.

---
+++ 1/While Do While/ForWhileDoWhile/src/Exer1.java
@@ -0,0 +1,18 @@
+public class Exer1 {
+    public static void main(String[] args) {
+
+        for (int i = 15; i <= 200; i++) {
+            int quadrado = i * i;
+            System.out.println("O quadrado de " + i + " é " + quadrado);
+        }
+    }
+}
+
+// variável int i = 15;
+// condição i <= 200; enquanto i <= 200; vai repetir
+// i++; vai tirar de 1 em 1 e é um final para que não seja infinito. Uma
+// contagem regressiva.
+// int quadrado = i * i; calcula o quadrado do número atual.
+// Quando iniciado, o laço vai percorrer todos os números de 15 a 200.
+
+// 1. Apresentar os quadrados dos números inteiros de 15 a 200.//
